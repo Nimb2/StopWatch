995e427f496e87339b2c5fc3d5429adc555e01f6
shnayder.1206 <shnayder.1206@gmail.com>
2023-11-17 23:15:35 +0700

    Восстановлены значения из объекта Bundle при повороте экрана

--- app/src/main/java/com/shnayder/android/stopwatch/MainActivity.kt
+++ app/src/main/java/com/shnayder/android/stopwatch/MainActivity.kt
@@ -11,12 +11,28 @@ class MainActivity : AppCompatActivity() {
     var running = false // Хронометр работает?
     var offset: Long = 0 //Базовое смещение
 
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Получение ссылки на секундомер
         stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        //Восстановление предыдущего состояния
+        if (savedInstanceState != null) {
+            //Восстановить значения из объекта Bundle
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            //Установить время на секундомере и запустить его, если он должен  выполняться.
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
 
         //Кнопка start запускает секундомер, если он не работал
         val startButton = findViewById<Button>(R.id.start_button)
@@ -44,6 +60,15 @@ class MainActivity : AppCompatActivity() {
             setBaseTime()
         }
     }
+
+    //помещение значений в объект Bundle (сохранение значений при повороте экрана)
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
     //Обновляет время stopwatch.base
     fun setBaseTime() {
         stopwatch.base = SystemClock.elapsedRealtime() - offset
